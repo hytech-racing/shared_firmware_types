0befdd3d4ac6d51839d78e2b25c240f578b518d2
songyueli <songyuedavidli@gmail.com>
2025-04-25 11:37:02 -0400

    added fw version info into acu all data struct

--- include/SharedFirmwareTypes.h
+++ include/SharedFirmwareTypes.h
@@ -428,6 +428,7 @@ struct ACUAllData_s
     size_t max_consecutive_invalid_packet_count;
     volt measured_tractive_system_voltage; 
     volt measured_pack_voltage;
+    FWVersionInfo fw_version_info;
     std::array<size_t, num_chips> consecutive_invalid_packet_counts;
     std::array<volt, num_cells> cell_voltages;
     std::array<celsius, num_cell_temps> cell_temps;  
